bdab7e8c3cc7b9c67c46500e467af67c373be1b9
kayjballer <jacquesbadji9@gmail.com>
2026-09-24 12:07:29 +0000

    chore: .gitignore pour backups locaux

--- .backup_agents_20260924/__init__.py
+++
@@ -1,5 +0,0 @@
-from .base import Agent, AgentDB, sans_accents
-from .models_catalog import CATALOG, total_mo, humain
-from .downloader import ModelDownloader
-from .model_manager import ModelManager
-from .director import Director, build_default_director
